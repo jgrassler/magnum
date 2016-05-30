5f1887a1ffa377bbfa8cd7a0b4f609c60b96d5bb
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-05-30 17:18:34 +0900

    Support trustee_domain_name in config file
    To setup magnum easier, name based configuration is important.
    This patch makes config file support trustee_domain_name and
    trustee_domain_admin_name in trust section.
    If name and id of trustee domain are provided by user, both
    values are passed through into keystone.
    
    Closes-Bug: #1581372
    Change-Id: Ia691aca7c29a471f6ba36a1a371ec1edf830b365
    (cherry picked from commit 1d748491938c377f43fc8baac666be37fa2eaad7)
    
    Conflicts:
    magnum/common/keystone.py
      Merged manually.
    magnum/conductor/template_definition.py
      Merged manually.
    magnum/tests/unit/conductor/handlers/test_k8s_bay_conductor.py
      Merged manually.
    magnum/tests/unit/conductor/handlers/test_swarm_bay_conductor.py
      Merged manually.

--- magnum/conductor/template_definition.py
+++ magnum/conductor/template_definition.py
@@ -66,7 +66,6 @@ template_def_opts = [
 
 CONF = cfg.CONF
 CONF.register_opts(template_def_opts, group='bay')
-CONF.import_opt('trustee_domain_id', 'magnum.common.keystone', group='trust')
 
 
 class ParameterMapping(object):
@@ -329,6 +328,8 @@ class TemplateDefinition(object):
 class BaseTemplateDefinition(TemplateDefinition):
     def __init__(self):
         super(BaseTemplateDefinition, self).__init__()
+        self._osc = None
+
         self.add_parameter('ssh_key_name',
                            baymodel_attr='keypair_id',
                            required=True)
@@ -349,9 +350,16 @@ class BaseTemplateDefinition(TemplateDefinition):
     def template_path(self):
         pass
 
+    def get_osc(self, context):
+        if not self._osc:
+            self._osc = clients.OpenStackClients(context)
+        return self._osc
+
     def get_params(self, context, baymodel, bay, **kwargs):
+        osc = self.get_osc(context)
+
         extra_params = kwargs.pop('extra_params', {})
-        extra_params['trustee_domain_id'] = CONF.trust.trustee_domain_id
+        extra_params['trustee_domain_id'] = osc.keystone().trustee_domain_id
         extra_params['trustee_user_id'] = bay.trustee_user_id
         extra_params['trustee_username'] = bay.trustee_username
         extra_params['trustee_password'] = bay.trustee_password
@@ -481,6 +489,12 @@ class K8sTemplateDefinition(BaseTemplateDefinition):
                 scale_mgr.get_removal_nodes(hosts))
 
         extra_params['discovery_url'] = self.get_discovery_url(bay)
+        osc = self.get_osc(context)
+        extra_params['magnum_url'] = osc.magnum_url()
+
+        if baymodel.tls_disabled:
+            extra_params['loadbalancing_protocol'] = 'HTTP'
+            extra_params['kubernetes_port'] = 8080
 
         label_list = ['flannel_network_cidr', 'flannel_use_vxlan',
                       'flannel_network_subnetlen']
@@ -515,7 +529,7 @@ class AtomicK8sTemplateDefinition(K8sTemplateDefinition):
 
         extra_params['username'] = context.user_name
         extra_params['tenant_name'] = context.tenant
-        osc = clients.OpenStackClients(context)
+        osc = self.get_osc(context)
         extra_params['user_token'] = self._get_user_token(context, osc, bay)
         extra_params['magnum_url'] = osc.magnum_url()
 
@@ -593,7 +607,7 @@ class AtomicSwarmTemplateDefinition(BaseTemplateDefinition):
         # HACK(apmelton) - This uses the user's bearer token, ideally
         # it should be replaced with an actual trust token with only
         # access to do what the template needs it to do.
-        osc = clients.OpenStackClients(context)
+        osc = self.get_osc(context)
         extra_params['user_token'] = self._get_user_token(context, osc, bay)
         extra_params['magnum_url'] = osc.magnum_url()
 
@@ -652,7 +666,7 @@ class UbuntuMesosTemplateDefinition(BaseTemplateDefinition):
         # HACK(apmelton) - This uses the user's bearer token, ideally
         # it should be replaced with an actual trust token with only
         # access to do what the template needs it to do.
-        osc = clients.OpenStackClients(context)
+        osc = self.get_osc(context)
         extra_params['auth_url'] = context.auth_url
         extra_params['username'] = context.user_name
         extra_params['tenant_name'] = context.tenant
